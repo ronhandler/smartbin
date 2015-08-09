b758af8cc4bcb181ab36a2b9a4f4858c1189fbc7
ronhandler <ronhandler@gmail.com>
2015-08-09 07:57:17 +0300

    Added the updated express + bootstrap project.

---
+++ routes/index.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var router = express.Router();
+
+var mytitle="פח חכם";
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: mytitle, currentpage: 'Home' });
+});
+
+router.get('/code', function(req, res, next) {
+  res.render('code', { title: mytitle, currentpage: 'Code' });
+});
+
+router.get('/about', function(req, res, next) {
+  res.render('about', { title: mytitle, currentpage: 'About' });
+});
+
+router.get('/contact', function(req, res, next) {
+  res.render('contact', { title: mytitle, currentpage: 'Contact' });
+});
+
+module.exports = router;
